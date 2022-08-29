8e1b2d24394dd0c1139a5756bad6ce9f580307be
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-08-29 08:25:00 +0530

    style: just rearranged function for better look

--- app.js
+++ app.js
@@ -15,21 +15,12 @@ const dateEntered = {
     year: 2022
 };
 
-console.log(checkPalindromeForAllFormats(dateEntered));
-
-function reverseString(textReceived) {
-    const reversedText = textReceived.split("").reverse().join("");
-    return reversedText;
-}
-
-function isPalindrome(textReceived) {
-    const reversedText = reverseString(textReceived);
-    return (reversedText === textReceived);
-}
+checkPalindromeForAllFormats(dateEntered);
+console.log(isLeapYear(dateEntered.year));
 
 function checkPalindromeForAllFormats(dateEnteredByUser) {
-    const usersDate = convertDateToString(dateEnteredByUser);
-    const datesFormatReceived = formatDate(usersDate);
+    
+    const datesFormatReceived = formatDate(dateEnteredByUser);
 
     let resultPalindrome = false;
     for (let currentFormat of datesFormatReceived) {
@@ -41,36 +32,62 @@ function checkPalindromeForAllFormats(dateEnteredByUser) {
     return resultPalindrome;
 }
 
-function convertDateToString(dateReceived) {
-    let stringDate = {
-        day: "",
-        month: "",
-        year: ""
-    };
-
-    if (dateReceived.day < 10) {
-        stringDate.day = "0" + dateReceived.day;
-    } else {
-        stringDate.day = String(dateReceived.day);
-    }
-
-    if (dateReceived.month < 10) {
-        stringDate.month = "0" + dateReceived.month;
-    } else {
-        stringDate.month = String(dateReceived.month);
-    }
-    stringDate.year = String(dateReceived.year);
-
-    return stringDate;
-}
-
-function formatDate(dateReceived) {
+function formatDate(dateEnteredByUser) {
+    const dateReceived = convertDateToString(dateEnteredByUser);
     const DDMMYYYY = dateReceived.day + dateReceived.month + dateReceived.year;
     const MMDDYYYY = dateReceived.month + dateReceived.day + dateReceived.year;
     const YYYYMMDD = dateReceived.year + dateReceived.month + dateReceived.day;
     const DDMMYY = dateReceived.day + dateReceived.month + dateReceived.year.slice(-2);
     const MMDDYY = dateReceived.month + dateReceived.day + dateReceived.year.slice(-2);
     const YYMMDD = dateReceived.year.slice(-2) + dateReceived.month + dateReceived.day;
-    formats = [DDMMYYYY, MMDDYYYY, YYYYMMDD, DDMMYY, MMDDYY, YYMMDD];
-    return formats;
+    const formatsDate = [DDMMYYYY, MMDDYYYY, YYYYMMDD, DDMMYY, MMDDYY, YYMMDD];
+    return formatsDate;
+}
+
+function convertDateToString(dateEnteredByUser) {
+    let stringDate = {
+        day: "",
+        month: "",
+        year: ""
+    };
+
+    if (dateEnteredByUser.day < 10) {
+        stringDate.day = "0" + dateEnteredByUser.day;
+    } else {
+        stringDate.day = String(dateEnteredByUser.day);
+    }
+
+    if (dateEnteredByUser.month < 10) {
+        stringDate.month = "0" + dateEnteredByUser.month;
+    } else {
+        stringDate.month = String(dateEnteredByUser.month);
+    }
+    stringDate.year = String(dateEnteredByUser.year);
+
+    return stringDate;
+}
+
+function reverseString(textReceived) {
+    const reversedText = textReceived.split("").reverse().join("");
+    return reversedText;
+}
+
+
+function isPalindrome(textReceived) {
+    const reversedText = reverseString(textReceived);
+    return (reversedText === textReceived);
+}
+
+function isLeapYear(year){
+    
+    if (year % 400 === 0){
+        return true;
+    }
+    if (year % 100 === 0){
+        return false;
+    }
+    if (year % 4 === 0){
+        return true;
+    }
+    return false;
 }
